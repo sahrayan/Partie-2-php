ca2cc43193af27d4762f5494172566ad921fc3d8
Rayan <rayanraihani9@gmail.com>
2022-11-14 10:53:34 +0100

    exo1

--- exercice-1-php2.php
+++ exercice-1-php2.php
@@ -1,10 +1,19 @@
+<style>
+    .red {
+        color: red;
+    }    
+</style>
+
 <?php
-$text = "Rayan la street";
+$text = "Mon texte en paramètre";
+$rayan = "rayan la street";
+ echo convertirMajRouge($text);
+ echo convertirMajRouge($rayan);
+function convertirMajRouge($text) {
 
-function convertirMajRouge($t){
-    strtoupper($t);
-    echo "<font color='red'>.$t.</font>";
+    $maj = mb_strtoupper($text); // convertir en maj
+    $maj = "<p class='red'>". $maj. "</p>"; //couleur rouge
+    return $maj;
+
 }
-convertirMajRouge($text);
-
 ?>
