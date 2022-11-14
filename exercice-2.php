cdc0e42a8a6185f9a624203298d5a9debdefe47a
Rayan <rayanraihani9@gmail.com>
2022-11-14 16:40:18 +0100

    exo 2 + 3

---
+++ exercice-2.php
@@ -0,0 +1,71 @@
+<h1>Exercice </h1>
+
+<!---
+<table border="1">
+    <thead>
+        <tr>
+            <th>Pays</th>
+            <th>Capitales</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>France</td>
+            <td>Paris</td>
+        </tr>
+        <tr>
+            <td>Allemagne</td>
+            <td>Berlin</td>
+        </tr>
+        <tr>
+            <td>USA</td>
+            <td>Washington</td> 
+        </tr>
+        <tr>
+            <td>Italie</td>
+            <td>Rome</td>
+        </tr>
+    </tbody>
+</table>
+---->
+<?php
+
+$capitales = [
+    "France"=>"Paris",
+    "Allemagne"=>"Berlin",
+    "USA"=>"Washington",
+    "Italie"=>"Rome"
+];
+
+ foreach($capitales as $pays => $capitale){
+    echo "le pays $pays a pour capitale : $capitale<br>";
+
+
+ }
+
+
+
+
+echo afficherTableHtml($capitales);
+function afficherTableHtml($capitales) {
+    $result = "<table border=1>
+                <thead>
+                    <tr>
+                        <th>Pays</th>
+                     <th>Capitales</th>
+                    </tr>
+                </thead>
+          <tbody>" ;
+foreach ($capitales as $pays => $capitale){
+    $result .= "<tr>
+                    <td>$pays</td> 
+                    <td>$capitale</td>
+                </tr>";
+
+}
+
+$result .= "</tbody></table>";
+return $result;
+}
+
+?>
---
+++ exo-3.php
@@ -0,0 +1,8 @@
+<h1>Exo 3</h1>
+<h2>Lien</h2>	
+<p>Voici le lien <a href="https://elan-formation.eu/accueil" target="_blank" rel="noopener noreferrer">clique</a>.</p>
+					
+<?php
+$link = "https://elan-formation.eu/accueil";
+echo '<p>Voici le lien <a href="' .$link. '" target="_blank" rel="noopener noreferrer">clique</a>.</p>';
+?>
