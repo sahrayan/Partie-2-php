88f01546686a6c1ede7b69acd68ee8a88a862060
sahrayan <113061570+sahrayan@users.noreply.github.com>
2022-11-17 16:45:09 +0100

    exo8

---
+++ exo8.php
@@ -0,0 +1,8 @@
+<img class="fit-picture"
+     src="$lien"
+/>
+     <?php
+$lien = "http://my.mobirise.com/data/userpic/764.jpg";
+$repeat = 4;
+echo str_repeat('<img src ="'.$lien.'"/>', $repeat);
+?>
