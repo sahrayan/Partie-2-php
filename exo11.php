a82ab39d0c375b7e68f0b6e35122c8d2c7e6f535
Rayan R <rayanraihani9@gmail.com>
2022-11-21 11:26:50 +0100

    exo 11

--- exo11.php
+++ exo11.php
@@ -1,5 +1,11 @@
 
 <?php
-echo date("d/m/Y"); // Affiche la date du jour
+setlocale(LC_Time, 'fr_FR');
+date_default_timezone_set('Europe/Paris');
+$date="27-01-2004"
+
+$formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+echo $formatter->format(date_create($date))."<br>";
+
 
 ?>
