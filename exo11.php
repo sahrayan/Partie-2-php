ad7321b51a2c77fd8e96db6b41d5df9192a37ec4
Rayan R <rayanraihani9@gmail.com>
2022-11-21 14:16:52 +0100

    exo11

--- exo11.php
+++ exo11.php
@@ -2,7 +2,7 @@
 <?php
 setlocale(LC_Time, 'fr_FR');
 date_default_timezone_set('Europe/Paris');
-$date="27-01-2004"
+$date="27-01-2004";
 
 $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
 echo $formatter->format(date_create($date))."<br>";
